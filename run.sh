8785f08e603bab542ef56b18140e22edd3ce64bf
Nathaniel Hobbs <hobbs.nathaniel@gmail.com>
2025-01-14 13:30:46 -0500

    changed port and subdomain temperarily

--- run.sh
+++ run.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-streamlit run app.py --server.port 3759
+#streamlit run app.py --server.port 3759
 
-echo "To see the webpage, navigate to: mt_scores.languaneers.com"
+#echo "To see the webpage, navigate to: mt_scores.languaneers.com"
+
+streamlit run app.py --server.port 3677
+
+echo "To see webpage, navigate to: embedding_analysis.languaneers.com"
+
+e
